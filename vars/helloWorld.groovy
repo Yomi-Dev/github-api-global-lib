ac26373d75f93d40ccd281dc46e86e744ef0c4cc
Yomi-Dev <102952655+Yomi-Dev@users.noreply.github.com>
2024-09-06 21:16:40 +0100

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -2,5 +2,5 @@ def call(Map config = [:]) {
     def name = config.get('name', 'Guest')  // Default value 'Guest' if name is not provided
     def day = config.get('day', 'today')    // Default value 'today' if day is not provided
 
-    echo "Hello, ${name}. Today is ${day}."
+    echo "Hello, ${name}. Today is ${dayOfWeek}."
 }
